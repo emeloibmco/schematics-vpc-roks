b1bdf0fc447fa095091f8065851f770316751fd1
diegorom27 <diegorom27@gmail.com>
2024-05-21 18:24:56 -0500

    .

--- vars.tf
+++ vars.tf
@@ -1,6 +1,10 @@
 ##############################################################################
 # Account variables
 ##############################################################################
+variable ibmcloud_api_key {
+    description = "The IBM Cloud platform API key needed to deploy IAM enabled resources"
+    type        = string
+}
 variable ibm_region {
     description = "IBM Cloud region where all resources will be deployed"
     type        = string
